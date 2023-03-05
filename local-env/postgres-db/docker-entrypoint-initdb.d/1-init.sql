900c7a302bada0c4bf35888c869eb5c03c2fe0b6
Barrett Kepler <45532267+kepler-br@users.noreply.github.com>
2023-03-05 21:36:41 +0300

    Liquibase wait for postgres ready

--- local-env/postgres-db/docker-entrypoint-initdb.d/1-init.sql
+++ local-env/postgres-db/docker-entrypoint-initdb.d/1-init.sql
@@ -1,9 +1,12 @@
-\c "postgres"
+-- \c "postgres"
 
 CREATE USER "user" WITH PASSWORD 'user';
 
+GRANT ALL ON SCHEMA public TO "user";
+-- GRANT USAGE, CREATE ON SCHEMA public TO user;
+-- \c "user"
+
 CREATE DATABASE "messages";
 
-GRANT ALL PRIVILEGES ON DATABASE "messages" TO "user";
+-- GRANT ALL ON DATABASE "messages" TO "user";
 
-GRANT ALL ON SCHEMA public TO "user";
