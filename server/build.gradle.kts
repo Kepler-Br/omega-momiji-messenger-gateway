18e27d457278a14ec360e7a29350087e5edd86eb
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2023-03-14 21:48:23 +0300

    Bot configuration property

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
 }
 
 dependencies {
+    val botApiVersion: String by project
+
     implementation(project(":${rootProject.name}-api"))
+    implementation("com.momiji.bot:omega-momiji-bot-api:$botApiVersion")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
--- server/src/main/kotlin/com/momiji/gateway/config/FrontendContainerConfiguration.kt
+++ server/src/main/kotlin/com/momiji/gateway/config/FrontendContainerConfiguration.kt
@@ -1,25 +1,25 @@
 package com.momiji.gateway.config
 
+import com.momiji.bot.api.ReceiveMessageEventsApi
+import com.momiji.gateway.config.properties.MomijiConfigurationProperties
 import com.momiji.gateway.frontend.FrontendContainer
 import com.momiji.gateway.frontend.api.FrontendApi
 import feign.Contract
 import feign.Feign
 import feign.codec.Decoder
 import feign.codec.Encoder
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.cloud.openfeign.FeignClientsConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
-data class FrontendConfig(
-    val url: String,
-)
-
-@ConfigurationProperties("momiji")
-data class ConfiguredFrontends constructor(
-    var frontends: Map<String, FrontendConfig>,
-)
+class BotClientsContainer(
+    private val bots: List<ReceiveMessageEventsApi>
+) {
+    fun getBots(): List<ReceiveMessageEventsApi> {
+        return bots
+    }
+}
 
 @Configuration
 // TODO: What FeignClientsConfiguration do?
@@ -28,7 +28,7 @@ class FrontendContainerConfiguration {
 
     @Bean
     fun frontendContainer(
-        config: ConfiguredFrontends,
+        config: MomijiConfigurationProperties,
         contract: Contract,
         decoder: Decoder,
         encoder: Encoder,
@@ -36,13 +36,28 @@ class FrontendContainerConfiguration {
         val clients = HashMap<String, FrontendApi>()
 
         for (frontend in config.frontends) {
-            clients[frontend.key] = Feign.builder()
-                .encoder(encoder)
-                .decoder(decoder)
-                .contract(contract)
+            clients[frontend.key] = Feign.builder().encoder(encoder).decoder(decoder).contract(contract)
                 .target(FrontendApi::class.java, frontend.value.url)
         }
 
         return FrontendContainer(clients)
+    }
+
+    @Bean
+    fun botClientContainer(
+        config: MomijiConfigurationProperties,
+        contract: Contract,
+        decoder: Decoder,
+        encoder: Encoder,
+    ): BotClientsContainer {
+        val clients = config.bots.map {
+            Feign.builder()
+                .encoder(encoder)
+                .decoder(decoder)
+                .contract(contract)
+                .target(ReceiveMessageEventsApi::class.java, it)
+        }
+
+        return BotClientsContainer(clients)
     }
 }
