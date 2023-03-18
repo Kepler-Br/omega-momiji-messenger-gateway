f909f37e8fef2323131a790a8c5e685579c4291f
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2023-03-18 14:55:51 +0300

    Add new media type

--- server/src/main/kotlin/com/momiji/gateway/mapper/MessageMapper.kt
+++ server/src/main/kotlin/com/momiji/gateway/mapper/MessageMapper.kt
@@ -1,17 +1,33 @@
 package com.momiji.gateway.mapper
 
 import com.momiji.api.gateway.inbound.model.ReceivedMessage
+import com.momiji.api.gateway.inbound.model.enumerator.MediaType
+import com.momiji.gateway.repository.entity.enumerator.MediaType as EntityMediaType
 import com.momiji.gateway.repository.entity.MessageEntity
 import org.springframework.stereotype.Component
 
 @Component
 class MessageMapper {
 
-    fun map(message: ReceivedMessage): MessageEntity {
+    private fun map(value: MediaType?): EntityMediaType? {
+        return when (value) {
+            MediaType.STICKER -> EntityMediaType.STICKER
+            MediaType.AUDIO -> EntityMediaType.AUDIO
+            MediaType.VOICE -> EntityMediaType.VOICE
+            MediaType.PHOTO -> EntityMediaType.PHOTO
+            MediaType.VIDEO -> EntityMediaType.VIDEO
+            MediaType.GIF -> EntityMediaType.GIF
+            MediaType.VIDEO_NOTE -> EntityMediaType.VIDEO_NOTE
+            null -> null
+        }
+    }
+
+    fun map(value: ReceivedMessage): MessageEntity {
         return MessageEntity(
-            frontend = message.frontend,
-            nativeId = message.id,
-            text = message.text,
+            frontend = value.frontend,
+            nativeId = value.id,
+            text = value.text,
+            mediaType = map(value.mediaType),
         )
     }
 }
